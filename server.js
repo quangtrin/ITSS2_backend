637464b634a7891548762b80b46f80b6974848fc
quangtrin <tienvu123d@gmail.com>
2024-06-14 22:47:30 +0700

    mongo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 
 // MongoDB
 mongoose
-  .connect("mongodb+srv://tienvu123d:HR87stkB04uFkPdX@cluster0.tsidaly.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+  .connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
